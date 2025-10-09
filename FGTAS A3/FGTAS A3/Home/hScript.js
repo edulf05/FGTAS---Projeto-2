71c158b40644e253e3358003f96849bf1b5069ec
RianBPadilha <96261142+RianBPadilha@users.noreply.github.com>
2025-10-09 19:26:37 -0300

    Delete FGTAS A3/FGTAS A3 directory

--- FGTAS A3/FGTAS A3/Home/hScript.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('logout-btn').addEventListener('click', () => {
-    // Redireciona para a página de login na pasta 'Login'
-    window.location.href = '../Login/index.html'; 
-});
--- FGTAS A3/FGTAS A3/Login/lScript.js
+++
@@ -1,6 +0,0 @@
-const loginButton = document.getElementById('Login');
-
-loginButton.addEventListener('click', function(event) {
-    event.preventDefault(); 
-    window.location.href = '../Home/home.html';
-});
