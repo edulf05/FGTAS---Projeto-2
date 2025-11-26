101d081173f4b2d9098dd7b03da42e0b4eb3aff5
Rian Bagatini Padilha <1302320722@ulife.com.br>
2025-11-26 20:56:17 -0300

    docs: insert banco.sql and details

--- src/routes/cidadeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const cidadeController = require("../controllers/cidadeController");
-
-router.get("/", cidadeController.listar);
-router.get("/:id", cidadeController.buscarPorId);
-router.get("/ultima/cadastrada", cidadeController.buscarUltima);
-
-module.exports = router;
--- src/routes/clienteRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const clienteController = require("../controllers/clienteController");
-
-router.get("/", clienteController.listar);
-router.get("/:id", clienteController.buscarPorId);
-router.post("/", clienteController.inserir);
-
-module.exports = router;
